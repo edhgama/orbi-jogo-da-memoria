ba928b0483a4c7eb507f82c34224bd740bc94862
Edheyberg <edhgama@gmail.com>
2022-05-10 09:32:35 -0400

    fix: lightCOlor() - agora remove o destaque após tempo determinado

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
 let order = [];
 let clickedOrder = []
-let score = 0
+let score = 0;
 
 // 0 - verde, 1 - vermelho, 2- amarelo, 3-azul
 
-const dblue = document.querySelector('.dblue')
-const dred = document.querySelector('.dred')
-const dyellow = document.querySelector('.dyellow')
-const dgreen = document.querySelector('.dgreen')
+const dblue = document.querySelector('.dblue');
+const dred = document.querySelector('.dred');
+const dyellow = document.querySelector('.dyellow');
+const dgreen = document.querySelector('.dgreen');
 
 // cria ordem aleatoria
 let shuffleOrder = () => {
@@ -17,20 +17,20 @@ let shuffleOrder = () => {
 
     for (let inc in order) {
         let elementColor = createColorElement(order[inc])
-        lightColor(elementColor, Number(inc)+1);
+        lightColor(elementColor, Number(inc) +1);
     }
 
 }
 
 // acende prox cor 
 let lightColor = (element, tempo) => {
-    tempo = tempo * 1000;
-    setTimeout(()=>{
+    tempo = tempo * 500;
+    setTimeout( () => {
         element.classList.add('selected');
     }, tempo - 250)
-    setTimeout(()=>{
+    setTimeout( () => {
         element.classList.remove('selected');
-    }, tempo - 250)    
+    }, tempo + 300)    
 }
 
 let checkOrder = () => {
